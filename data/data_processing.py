ecf2b33520dc551b11e8185be5e01ed65a45b7f3
fillantrophy02 <49400054+fillantrophy02@users.noreply.github.com>
2025-04-17 23:34:27 +0800

    add "RainTomorrow" to test set (#2)
    * LSTM + trainer model
    
    * lstm one step update
    
    * lstm multi step update
    
    * others update
    
    * data_processing update
    
    ---------
    
    Co-authored-by: leowsimin <leowsmr@gmail.com>

--- data/data_processing.py
+++ data/data_processing.py
@@ -1,18 +1,22 @@
 import pandas as pd
+import torch
 
 class DataProcessingPipeline():
     def __init__(self, df: pd.DataFrame):
         self.df = df
 
-    def clean(self):
-        self._drop_rows_with_na_labels()
+    def clean(self, type):
+        #self._drop_rows_with_na_labels()
+        self._rain_today_na(type) #Train
+        self._drop_rows_with_na_labels(type)
         self._transform_categorical_features_to_numerical()
         self._extract_time_series_feature_for_city()
         self._transform_into_multi_index()
         self._drop_columns_with_too_many_missing_values()
-        self._drop_unnecessary_columns()
+        # self._drop_unnecessary_columns()
         self._interpolate_missing_values()
         self._transform_label_to_binary()
+        self._test_rain_tomorrow(type) #Test
 
     def get(self) -> pd.DataFrame:
         return self.df
@@ -59,8 +63,11 @@ class DataProcessingPipeline():
             if col in self.df.columns:
                 self.df.drop(columns=columns_to_drop, inplace=True)
 
-    def _drop_rows_with_na_labels(self):
-        self.df.dropna(subset=['RainToday'], inplace=True)
+    def _drop_rows_with_na_labels(self, type):
+        if type == 'Test':
+            self.df.dropna(subset=['RainToday'], inplace=True)
+        else:
+            pass
 
     def _interpolate_missing_values(self):
         self.df.interpolate(inplace=True, limit_direction='both')
@@ -71,29 +78,91 @@ class DataProcessingPipeline():
         self.df = self.df[first_two_columns + [col for col in self.df.columns if col not in first_two_columns]]
 
     def _transform_categorical_features_to_numerical(self):
-        cat_columns = ['RainToday', 'Location', 'WindGustDir', 'WindDir9am', 'WindDir3pm']
+
+        if 'RainToday' in self.df.columns:
+            self.df['RainToday'] = self.df['RainToday'].map(lambda x: 1 if x == 'Yes' else (0 if x == 'No' else x))
+
+        cat_columns = ['Location', 'WindGustDir', 'WindDir9am', 'WindDir3pm']
         existing_columns = [col for col in cat_columns if col in self.df.columns]
 
-        if existing_columns:
-            for col in existing_columns:
-                self.df[col] = self.df[col].astype('category').cat.codes
-                self.df[col] = self.df[col].replace(-1, self.df[col].max() + 1)  # Replace -1 with the next available number
+        for col in existing_columns:
+            self.df[col] = self.df[col].astype('category').cat.codes
+
 
     def _transform_label_to_binary(self):
         pass
 
+    def _rain_today_na(self, type):
+        if type == "Train":
+            for i in range(1, len(self.df)):
+                if pd.isna(self.df.loc[i, "RainToday"]):
+                    self.df.loc[i, "RainToday"] = self.df.loc[i - 1, "RainTomorrow"]
+        else:
+            pass
+
+    def prepare_tensor_data(self):
+
+        features_columns = self.df.columns[:-1]
+        target_column = self.df.columns[-1]
+
+        X = self.df[features_columns].values
+        Y = self.df[target_column].values
+
+        X_tensor = torch.tensor(X, dtype=torch.float32)
+        Y_tensor = torch.tensor(Y, dtype=torch.float32)
+
+        return X_tensor, Y_tensor
+    
+    def create_sequences(self, X, Y, seq_len):
+
+        n_samples = X.shape[0]
+        n_features = X.shape[1]
+        
+        X_seq = []
+        Y_seq = []
+        
+        for i in range(n_samples - seq_len):
+            X_seq.append(X[i:i+seq_len])
+            Y_seq.append(Y[i:i+seq_len])
+        
+        X_seq = torch.stack(X_seq)
+        Y_seq = torch.stack(Y_seq)
+        
+        return X_seq, Y_seq
+    
+    def na_values(self):
+        na_columns = self.df.columns[self.df.isna().any()].tolist()
+        
+        if na_columns:
+            print("Columns with NA values:")
+            for col in na_columns:
+                na_count = self.df[col].isna().sum()
+                print(f"- {col}: {na_count} missing")
+        else:
+            print("No columns have NA vsalues.")
+
+    def _test_rain_tomorrow(self, type):
+        # This assumes that in the last data, it will not rain the next day!
+        if type == "Test":
+            self.df['RainTomorrow'] = self.df['RainToday'].shift(-1).fillna(0).astype(int)
+        else:
+            pass
+
+
+type = "Train"
 df = pd.read_csv('data/raw-data/train.csv')
 pipeline = DataProcessingPipeline(df)
 pipeline.report()
-pipeline.clean()
+pipeline.clean(type)
 print("\nAfter cleaning ----------------------------------")
 pipeline.report()
 pipeline.export_to_csv('data/processed-data/train.csv')
 
+type = "Test"
 df = pd.read_csv('data/raw-data/test.csv')
 pipeline = DataProcessingPipeline(df)
 pipeline.report()
-pipeline.clean()
+pipeline.clean(type)
 print("\nAfter cleaning ----------------------------------")
 pipeline.report()
 pipeline.export_to_csv('data/processed-data/test.csv')
